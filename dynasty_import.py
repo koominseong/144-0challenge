fd808cfb5635fd1b8e7b38e6048ca222276d569a
Minseong Koo <bestms8921@gmail.com>
2026-07-10 14:34:54 +0900

    Update dynasty_import.py

--- dynasty_import.py
+++ dynasty_import.py
@@ -21,7 +21,7 @@ YEARS_PER_SEASON = 3
 
 # 최소 보장 인원 (부족하면 랜덤 생성으로 채움)
 MIN_PLAYERS_FIRST = 280   # 10팀 × 25라운드 = 250 + 여유
-MIN_PLAYERS_NEXT = 50     # 신인 드래프트 풀
+MIN_PLAYERS_NEXT = 150     # 신인 드래프트 풀
 
 BATTER_POS = {"C", "1B", "2B", "3B", "SS", "LF", "CF", "RF", "DH", "OF", "IF"}
 PITCHER_POS = {"P", "SP", "RP", "CP"}
